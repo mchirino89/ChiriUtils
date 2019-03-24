d511903def95def67e024243d94748b0b35e5837
Mauricio Chirino <mchirino@pch.com>
2019-03-24 17:22:06 -0300

    Add register cell helper

--- Library/ChiriUtils/Utils/Extensions.swift
+++ Library/ChiriUtils/Utils/Extensions.swift
@@ -6,13 +6,20 @@
 //  Copyright © 2019 Mauricio Chirino. All rights reserved.
 //
 
-import Foundation
-
 extension UITableView {
+
     func deque<T: UITableViewCell>(for id: String = String(describing: T.self)) -> T {
         guard let dequeued = self.dequeueReusableCell(withIdentifier: id) as? T else {
             preconditionFailure("Check your UITableViewCell settings for \(T.self)")
         }
         return dequeued
+    }
+
+    // Check this for futher details https://www.swiftbysundell.com/posts/enum-iterations-in-swift-42
+    func registerCellClasses<T: CaseIterable & RawRepresentable>(for sectionType: T.Type,
+                                                                 using resolver: (T) -> UITableViewCell.Type) where T.RawValue == String {
+        for section in sectionType.allCases {
+            register(resolver(section), forCellReuseIdentifier: section.rawValue)
+        }
     }
 }
