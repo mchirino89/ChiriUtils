56cf0b960f13b61800f77efe14911e57a38c66ea
Mauricio Chirino <mauricio@3codegeeks.com>
2019-05-26 20:03:44 -0300

    Remove syntactic sugar for Result

--- Library/ChiriUtils/Utils/Extensions.swift
+++ Library/ChiriUtils/Utils/Extensions.swift
@@ -10,11 +10,11 @@ import UIKit
 
 public extension UICollectionView {
 
-    public func register(cellType: AnyClass) {
+    func register(cellType: AnyClass) {
         self.register(cellType.self, forCellWithReuseIdentifier: String(describing: cellType.self))
     }
 
-    public func deque<T: UICollectionViewCell>(for id: String = String(describing: T.self),
+    func deque<T: UICollectionViewCell>(for id: String = String(describing: T.self),
                                         at indexPath: IndexPath) -> T {
         guard let dequeued = self.dequeueReusableCell(withReuseIdentifier: id, for: indexPath) as? T else {
             preconditionFailure("Check your UICollectionViewCell settings for \(T.self)")
@@ -25,11 +25,11 @@ public extension UICollectionView {
 
 public extension UITableView {
 
-    public func register(cellType: AnyClass) {
+    func register(cellType: AnyClass) {
         self.register(cellType.self, forCellReuseIdentifier: String(describing: cellType.self))
     }
 
-    public func deque<T: UITableViewCell>(for id: String = String(describing: T.self)) -> T {
+    func deque<T: UITableViewCell>(for id: String = String(describing: T.self)) -> T {
         guard let dequeued = self.dequeueReusableCell(withIdentifier: id) as? T else {
             preconditionFailure("Check your UITableViewCell settings for \(T.self)")
         }
--- README.md
+++ README.md
@@ -5,8 +5,6 @@ This helper [library](https://cocoapods.org/pods/ChiriUtils) includes some of th
 
 - `JSONHelper`: read locally stored files within your project or inside the app's bundle with this _factory_ class.
 
-- `Result`: make your API calls type safe using this `enum` so that all states are handled. Optionally, convert all JSON success response data into your custom _Decodable_ models (backwards compatibility with Swift 4).
-
 - And more: deque your `UITableView`/`UICollectionView` in a typesafe way. Avoid the _guard-let-else_ dance altogether or even worse: the infamaus force casting **!**
 
 Feel free to submit your pull requests and I'll happily check them. Thanks!
